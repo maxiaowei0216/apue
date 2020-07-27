886b7f8bc1c73042f328bb27cd368ca6d50fd0e1
maxiaowei <maxiaowei0216@gmail.com>
2020-07-27 15:09:00 +0800

    添加自动编译运行的脚本

---
+++ start.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+cd ./build
+rm -r *
+cmake ..
+# 检查cmake是否错误
+if [ $? -eq 0 ]
+then
+    make
+    # 检查make是否错误
+    if [ $? -eq 0 ]
+    then
+        ./bin/exc
+    else
+        echo "make error"
+    fi
+else
+    echo "cmake error"
+fi
